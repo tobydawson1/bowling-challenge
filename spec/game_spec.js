c298111d43111753e4df397495716604d71719ee
Toby Dawson <tobydawson1@gmail.com>
2020-02-22 17:01:26 +0000

    added score and frame functionality

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -3,16 +3,50 @@ describe('game', function () {
 
     beforeEach(function() {
         game = new Game();
-        
     });
 
-
-    it('is a game', function() {
-        game
+    it('records scores', function() {
+        game.bowl(10)
+        game.bowl(5)
+        expect(game.scores).toEqual([10, 5])
     });
 
-    it('takes a score', function() {
-        expect(game.bowl(10)).toEqual(10)
+    it('record a frame', function() {
+        game.bowl(4)
+        game.bowl(5)
+        expect(game.frames).toEqual([[4, 5]])
     });
+
+    it('records a strike', function() {
+        game.bowl(10)
+        expect(game.frames).toEqual([[10]])
+    })
+
+    it('ends the game', function() {
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(9)
+        game.bowl(0)
+        expect(game.endGame()).toEqual(true)
+    })
+
+    it('playes until the end', function() {
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+        game.bowl(10)
+      
+        expect(game.endGame()).toEqual(false)
+    })
 
 });
--- src/game.js
+++ src/game.js
@@ -1,6 +1,31 @@
 function Game() {
     
-    Game.prototype.bowl = function(score) {
-        return score
-    }
+    this.scores = []
+    this.frames = []
+    this.previousScore = undefined
+    
 }
+
+Game.prototype.bowl = function(score) {
+    
+    this.scores.push(score);  
+    if(this.previousScore !== undefined) {
+        this.frames.push([this.previousScore, score])
+        this.previousScore = undefined 
+    } else 
+        if (score == 10) 
+            this.frames.push([score])
+        else
+            this.previousScore = score
+}
+
+Game.prototype.endGame = function() {
+    return this.frames.length === 10
+        
+}
+
+
+
+
+
+
